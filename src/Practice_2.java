62724a7f790c74b707b707766dc226f8149f3521
Darya Korotkih <riyafonte@yahoo.com>
2017-11-11 23:48:05 +0300

    guitar picture

---
+++ src/Practice_2.java
@@ -0,0 +1,9 @@
+public class Practice_2 {
+    public static void main(String[] args) {
+        System.out.println("░▄▀▀▀▀▄░░▄▄\n" +
+                "█░░░░░░▀▀░░█░░░░░░▄░▄\n" +
+                "█░║░░░░██░████████████\n" +
+                "█░░░░░░▄▄░░█░░░░░░▀░▀\n" +
+                "░▀▄▄▄▄▀░░▀▀");
+    }
+}
